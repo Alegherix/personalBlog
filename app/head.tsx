725a94fd2c0f56a453112754566d63faa2372dbc
Martin <alegherix@gmail.com>
2022-11-19 00:51:50 +0100

    Added Image of me to meta

--- app/head.tsx
+++ app/head.tsx
@@ -7,6 +7,10 @@ export default function Head() {
         name="description"
         content="The personal blog of Alegherix where I share my interests such as what I'm currently reading, and what I'm currently learning. I might even throw in a little bit of code, who knows."
       />
+      <meta
+        property="og:image"
+        content="https://avatars.githubusercontent.com/u/29613297?v=4"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
